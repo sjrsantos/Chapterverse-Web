336342f0d2b825d30899fb55f39a9afbd9be2b79
Silvio Júnior <silvio-sjunior@hotmail.com>
2024-04-28 16:32:26 -0400

    fixing path

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Chapterverse-Web">
       <App />
     </BrowserRouter>
   </React.StrictMode>
